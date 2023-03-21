2ece554172fb18d264d1fd2b468cee07cc2d608d
Gennady Kashin <gennady.kashin94@gmail.com>
2023-03-21 09:22:51 +0300

    feat: add Header

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,18 +4,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
